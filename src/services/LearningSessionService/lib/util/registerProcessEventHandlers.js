0f5a2cab48dc3b446658912072963facbc543dcf
rZinnatov <roman.zinnatov@gmail.com>
2019-01-10 18:55:20 +0300

    Refactoring: Move process event handlers registration into separate file

---
+++ src/services/LearningSessionService/lib/util/registerProcessEventHandlers.js
@@ -0,0 +1,36 @@
+function registerProcessEventHandlers(api) {
+    async function stopService() {
+        try {
+            console.log(`RemoteML(${process.pid}): LearningSessionService is stopping`);
+            await api.stop();
+            console.log(`RemoteML(${process.pid}): LearningSessionService is stopped`);
+            process.exit(0);
+            
+        } catch(error) {
+            console.error(`RemoteML(${process.pid}): Error while stopping the service: '${error}'`);
+            process.exit(1);
+        }
+    };
+    async function signalHandler(signal) {
+        console.log(`RemoteML(${process.pid}): Signal '${signal}' is caught`);
+        await stopService();
+    }
+    async function uncaughtExceptionHandler(error) {
+        console.error(`RemoteML(${process.pid}): Unhandled exception: ${error}`);
+        await stopService();
+    }
+    async function unhandledRejectionHandler(reason, p) {
+        console.error(`RemoteML(${process.pid}): Unhandled rejection at: '${p}' with reason '${reason}'`);
+        await stopService();
+    }
+    
+    process.on('exit', (code) => console.log(`RemoteML(${process.pid}): Exit process with code '${code}'`));
+    process.on('SIGINT', signalHandler);
+    process.on('SIGTERM', signalHandler);
+    process.on('SIGHUP', signalHandler);
+    process.on('SIGBREAK', signalHandler);
+    process.on('uncaughtException', uncaughtExceptionHandler);
+    process.on('unhandledRejection', unhandledRejectionHandler);
+}
+
+module.exports = registerProcessEventHandlers;
--- src/services/LearningSessionService/run.js
+++ src/services/LearningSessionService/run.js
@@ -1,20 +1,7 @@
+const registerProcessEventHandlers = require('./lib/util/registerProcessEventHandlers');
 const LearningSessionServiceFactory = require('./index');
+
 const api = new LearningSessionServiceFactory().createNewApiInstance();
-
-
-process.on('exit', (code) => console.log(`RemoteML(${process.pid}): Exit process with code '${code}'`));
-process.on('SIGINT', async () => {
-    try {
-        await api.stop();
-        console.log(`RemoteML(${process.pid}): LearningSessionService is stopped`);
-        process.exit(0);
-
-    } catch(error) {
-        console.error(`RemoteML(${process.pid}): Error in SIGINT handler`);
-        console.error(error);
-        process.exit(1);
-    }
-});
-
+registerProcessEventHandlers(api);
 
 api.run();
