463b70e44a88156602fe993e014e47882b377293
Roman Zinnatov <roman.zinnatov@gmail.com>
2019-01-16 09:08:02 -0500

    Fix docker image stuff in travis

--- deploy/create-settings-file.sh
+++ deploy/create-settings-file.sh
@@ -1,12 +1,12 @@
-echo '{' \
-        '"api": {' \
-            '"port": $REMOTEML_PORT,' \
-            '"path": "$REMOTEML_API_PATH"' \
-        '},' \
-        '"storage": {' \
-            '"url": "$REMOTEML_MONGO_URL",' \
-            '"dbName": "$REMOTEML_MONGO_DB_NAME",' \
-            '"collectionName": "$REMOTEML_MONGO_COLLECTION_NAME"' \
-        '}' \
-    '}' \
+echo "{" \
+        "\"api\": {" \
+            "\"port\": $REMOTEML_PORT," \
+            "\"path\": \"$REMOTEML_API_PATH\"" \
+        "}," \
+        "\"storage\": {" \
+            "\"url\": \"$REMOTEML_MONGO_URL\"," \
+            "\"dbName\": \"$REMOTEML_MONGO_DB_NAME\"," \
+            "\"collectionName\": \"$REMOTEML_MONGO_COLLECTION_NAME\"" \
+        "}" \
+    "}" \
     > settings.json
