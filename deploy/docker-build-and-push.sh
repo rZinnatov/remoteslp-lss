393d137b2b1892ecb90b6e52fbed34d2521cb385
Roman Zinnatov <roman.zinnatov@gmail.com>
2019-01-16 09:21:32 -0500

    Update .dockerignore

--- deploy/docker-build-and-push.sh
+++ deploy/docker-build-and-push.sh
@@ -1,5 +1,5 @@
 IMAGE_NAME=rzinnatov/remoteml-lss:latest
 
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-docker build -f ./deploy/docker/Dockerfile -t $IMAGE_NAME .
+docker build -f ./deploy/Dockerfile -t $IMAGE_NAME .
 docker push $IMAGE_NAME
