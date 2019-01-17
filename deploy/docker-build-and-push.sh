d00fa4b6f1d1d9634fb55cdba8ffab4fe37f8362
Roman Zinnatov <roman.zinnatov@gmail.com>
2019-01-17 06:22:15 -0500

    Fix deploy scripts

--- deploy/docker-build-and-push.sh
+++ deploy/docker-build-and-push.sh
@@ -2,5 +2,5 @@ IMAGE_NAME=rzinnatov/remoteml-lss:latest
 DOCKERFILE_PATH="$(cd "$(dirname "$0")"; pwd)"/Dockerfile
 
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-docker build -f DOCKERFILE_PATH -t $IMAGE_NAME .
+docker build -f $DOCKERFILE_PATH -t $IMAGE_NAME .
 docker push $IMAGE_NAME
